3bc7982960d05075afebce0ef706fb8cc225d95f
Guido Stevens <guido.stevens@cosent.nl>
2012-11-15 08:40:48 -0500

    Preparing release 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3rc6'
+version = '0.3'
 
 setup(name='dummypackage',
       version=version,
@@ -24,3 +24,4 @@ setup(name='dummypackage',
       # -*- Entry points: -*-
       """,
       )
+ )
